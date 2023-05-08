2a31d84df10e40ed09a287ca4eb8c18a7d48da55
Paulo Matos <pauloclmatos@gmail.com>
2023-05-08 14:05:54 +0100

    first stuff

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hotncold/pages/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hot n' Cold",
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class LoginPage extends StatefulWidget {
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController pwdController = TextEditingController();
+
+  // ignore: prefer_typing_uninitialized_variables
+  var name;
+  // ignore: prefer_typing_uninitialized_variables
+  var pwd;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Hot N' Cold"),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 44, 124, 228),
+                  Color.fromARGB(255, 201, 42, 31)
+                ],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+                stops: [0.0, 1.0],
+                tileMode: TileMode.decal)),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  style: const TextStyle(color: Colors.white),
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    hintText: "Enter your name",
+                    hintStyle: TextStyle(color: Colors.white),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  style: const TextStyle(color: Colors.white),
+                  controller: pwdController,
+                  decoration: const InputDecoration(
+                    hintText: "Enter your password",
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      ElevatedButton(
+                        child: const Text("Login"),
+                        onPressed: () {
+                          setState(() {
+                            name = nameController.text;
+                            pwd = pwdController.text;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Column(children: [
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text("Register")),
+                  ])
+                ],
+              ),
+              const Text("Please register if you don't have an account."),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

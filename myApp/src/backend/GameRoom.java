51ad5b4532c49f620080d61c5884ea8f0d6dcae2
Paulo Matos <pauloclmatos@gmail.com>
2023-05-11 14:08:21 +0100

    starting webSocket

--- myApp/src/backend/GameRoom.java
+++ myApp/src/backend/GameRoom.java
@@ -2,8 +2,28 @@ package backend;
 
 import java.util.List;
 
-public class GameRoom {
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.Session;
+import javax.websocket.server.*;
+
+public class GameRoom extends Endpoint{
     
     private List<Account> players;
+
+    @Override
+    public void onOpen(Session session, EndpointConfig conf) {
+        ServerEndpointConfig.Builder.create(GameRoom.class, "/room");
+        session.addMessageHandler(new MessageHandler.Whole<String>() {
+
+            @Override
+            public void onMessage(String arg0) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'onMessage'");
+            }
+            
+        });
+    }
 
 }

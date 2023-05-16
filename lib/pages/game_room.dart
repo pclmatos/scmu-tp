37c43cf3c1ef4876dc9de9717d75b30694b01372
Paulo Matos <pauloclmatos@gmail.com>
2023-05-16 14:11:41 +0100

    maybe auto refreshable page working

--- lib/pages/game_room.dart
+++ lib/pages/game_room.dart
@@ -18,63 +18,65 @@ class GameRoom extends StatelessWidget {
     var roomState = context.watch<RoomState>();
     final MyUser user = Provider.of<MyUser>(context);
 
-    return Scaffold(
-      appBar: header(context, false),
-      body: Container(
-        constraints: const BoxConstraints(maxHeight: 700),
-        decoration: background(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.all(20),
-              child: Text(style: TextStyle(color: Colors.white), 'Players'),
-            ),
-            SizedBox(
-                height: 500,
-                child: ListView(
-                  children: [
-                    for (var player in roomState.players)
-                      ListTile(
-                        iconColor: Colors.white,
-                        leading: const Icon(Icons.person),
-                        title: Text(
-                            style: const TextStyle(color: Colors.white),
-                            player.email),
-                      )
-                  ],
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 20,
-                  width: 200,
-                  child: Text(
-                      style: const TextStyle(color: Colors.white),
-                      '${roomState.readyCount} out of ${roomState.players.length} players are ready!'),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    onPressed: () async {
-                      //await Connection().writeMessage('READY', PlayerEntry(user.email, 'READY'));
-                    },
-                    child: const Text("Ready")),
-                ElevatedButton(
-                    onPressed: () {
-                      Connection().closeConnection();
-                    },
-                    child: const Text("Leave"))
-              ],
-            )
-          ],
+    return Consumer<RoomState>(builder: (context, roomState, child) {
+      return Scaffold(
+        appBar: header(context, false),
+        body: Container(
+          constraints: const BoxConstraints(maxHeight: 700),
+          decoration: background(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(style: TextStyle(color: Colors.white), 'Players'),
+              ),
+              SizedBox(
+                  height: 500,
+                  child: ListView(
+                    children: [
+                      for (var player in roomState.players)
+                        ListTile(
+                          iconColor: Colors.white,
+                          leading: const Icon(Icons.person),
+                          title: Text(
+                              style: const TextStyle(color: Colors.white),
+                              player.email),
+                        )
+                    ],
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 20,
+                    width: 200,
+                    child: Text(
+                        style: const TextStyle(color: Colors.white),
+                        '${roomState.readyCount} out of ${roomState.players.length} players are ready!'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () async {
+                        //await Connection().writeMessage('READY', PlayerEntry(user.email, 'READY'));
+                      },
+                      child: const Text("Ready")),
+                  ElevatedButton(
+                      onPressed: () {
+                        Connection().closeConnection();
+                      },
+                      child: const Text("Leave"))
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }

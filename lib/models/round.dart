d7e946091313757a48dba20eec687ba65e70fa35
JoaoSergioAguero <61967521+JoaoSergioAguero@users.noreply.github.com>
2023-05-17 15:23:32 +0100

    working on rounds

---
+++ lib/models/round.dart
@@ -0,0 +1,31 @@
+import 'package:hotncold/models/player_entry.dart';
+
+class Round {
+  List<PlayerEntry> seekers = [];
+  final PlayerEntry hider;
+  final double latitude;
+  final double longitude;
+
+  Round(this.hider, this.seekers, this.latitude, this.longitude);
+
+  factory Round.fromJson(dynamic json) {
+    List<dynamic> playersTmp = json['players'] as List;
+
+    List<PlayerEntry> tmp = [];
+    for (var entry in playersTmp) {
+      tmp.add(PlayerEntry.fromJson(entry));
+    }
+
+    return Round(PlayerEntry.fromJson(json), tmp, json['latitude'] as double,
+        json['longitude'] as double);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'seekers': seekers,
+      'hider': hider,
+      'latitude': latitude,
+      'longitude': longitude
+    };
+  }
+}

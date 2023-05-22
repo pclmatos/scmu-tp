f82d7bf21ea22218dcdd67a37458b711cc4811d7
JoaoSergioAguero <61967521+JoaoSergioAguero@users.noreply.github.com>
2023-05-22 14:41:34 +0100

    camera W; able to take picture; need to send to server and store in database

--- lib/pages/in_game/hider.dart
+++ lib/pages/in_game/hider.dart
@@ -1,9 +1,28 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:hotncold/pages/in_game/camera.dart';
 import 'package:hotncold/pages/tools/header.dart';
 import 'package:hotncold/pages/tools/background.dart';
 
-class Hider extends StatelessWidget {
+class Hider extends StatefulWidget {
   const Hider({super.key});
+
+  @override
+  _HiderState createState() => _HiderState();
+}
+
+class _HiderState extends State<Hider> {
+  late List<CameraDescription> cameras;
+
+  Future<void> getCameras() async {
+    cameras = await availableCameras();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCameras();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +43,17 @@ class Hider extends StatelessWidget {
               ),
               print(
                   'Please hide the treasure and take a photo of its location.'),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Camera()));
+                  },
+                  child: const Text("Go to camera")),
             ],
           ),
         ),
